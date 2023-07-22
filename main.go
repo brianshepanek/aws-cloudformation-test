6473777b4f60dcd8cd242d32c3bbc3c91a7f3490
Brian Shepanek <brianshepanek@gmail.com>
2023-07-22 07:12:19 -0400

    Update with rand

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
 	"time"
 )
@@ -12,6 +13,7 @@ func main() {
 	http.HandleFunc("/bar", barHandler)
 	http.HandleFunc("/fizz", fizzHandler)
 	http.HandleFunc("/fuzz", fuzzHandler)
+	http.HandleFunc("/rand", fuzzHandler)
 	fmt.Println("Server is listening on port 8080...")
 	http.ListenAndServe(":80", nil)
 }
@@ -34,4 +36,44 @@ func fizzHandler(w http.ResponseWriter, r *http.Request) {
 
 func fuzzHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, Fuzz @ "+time.Now().String())
+}
+
+func randHandler(w http.ResponseWriter, r *http.Request) {
+	// Predefined sentences
+	sentences := []string{
+		"Hello, world!",
+		"The quick brown fox jumps over the lazy dog.",
+		"Programming in Go is fun.",
+		"AI is the future.",
+		"OpenAI has created the GPT model.",
+		"This is a sample sentence.",
+		"The sun rises in the east.",
+		"Be kind to others.",
+		"Hard work always pays off.",
+		"Success is the result of perseverance.",
+		"Honesty is the best policy.",
+		"Health is wealth.",
+		"The early bird catches the worm.",
+		"Practice makes perfect.",
+		"Rome was not built in a day.",
+		"Unity is strength.",
+		"Education is the key to success.",
+		"Time and tide wait for no man.",
+		"Action speaks louder than words.",
+		"All that glitters is not gold.",
+		"Where there's smoke, there's fire.",
+		"You can't judge a book by its cover.",
+		"When in Rome, do as the Romans do.",
+		"Every cloud has a silver lining.",
+		"Don't count your chickens before they're hatched.",
+	}
+
+	// Seed the random number generator
+	rand.Seed(time.Now().UnixNano())
+
+	// Get a random index
+	index := rand.Intn(len(sentences))
+
+	// Print the random sentence
+	fmt.Fprintf(w, sentences[index]+" @ "+time.Now().String())
 }
